7d02c1747f9a59b6696358f64e6181884e4d757d
Kevin Huang <linightz@hotmail.com>
2019-04-12 14:33:20 +0800

    Add comments

--- Arith_Geo_Sequence.py
+++ Arith_Geo_Sequence.py
@@ -1,3 +1,16 @@
+'''
+Written by KH 3/18/19
+
+The program first takes a int as how many input sequences, then takes the number of lines of 4 ints separated by spaces and calculates the 5th number depending on the previous 4 ints whether they form an arithmetic or a geometric sequence.
+Ex. input:
+2
+1 2 3 4
+2 4 8 16
+Ex. output:
+1 2 3 4 5
+2 4 8 16 32
+'''
+
 import sys
 for i in map(int, sys.stdin):
     for j in range(i):
