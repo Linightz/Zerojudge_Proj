783ea94ebfce534da4fbde3777b3d022b9fd6f6e
Kevin Huang <linightz@hotmail.com>
2019-04-12 14:45:18 +0800

    Add comment

--- Matrix_Rotation.cpp
+++ Matrix_Rotation.cpp
@@ -1,3 +1,17 @@
+/*
+Written by KH 10/3/17
+
+The program rotate an n * m matrix to a m * n matrix. It first takes two ints as n and m separated by space, then it takes n number of lines in which contains m number of ints separated by spaces. Finally the program outputs rotated matrix.
+Ex. input:
+2 3
+3 1 2
+8 5 4
+Ex. output:
+3 8
+1 5
+2 4
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
