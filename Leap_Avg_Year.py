34728794a8502315477f082a478a2c93ed6f71b7
Kevin Huang <linightz@hotmail.com>
2019-04-12 14:43:11 +0800

    Add comment

--- Leap_Avg_Year.py
+++ Leap_Avg_Year.py
@@ -1,3 +1,9 @@
+'''
+Written by KH 3/18/19
+
+The program takes one A.D. year (int) as input and output if it's a leap year or a average year.
+'''
+
 import sys
 for s in map(int, sys.stdin):
     if s%4==0 and s%100!=0 or s%400==0:
